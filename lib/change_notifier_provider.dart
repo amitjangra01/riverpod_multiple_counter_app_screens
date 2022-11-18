37a624027669d5048ca34fad166e7a3f8bd1e375
amitjangra579 <amitjangra579@gmail.com>
2022-11-18 10:38:18 +0530

    improvements

--- lib/change_notifier_provider.dart
+++ lib/change_notifier_provider.dart
@@ -7,6 +7,7 @@ class ChangeNotifierScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final changeNotifierCounter = ref.watch(changeNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Change Notifier Screen'),
@@ -23,11 +24,42 @@ class ChangeNotifierScreen extends ConsumerWidget {
               icon: const Icon(Icons.refresh))
         ],
       ),
-      body: Center(
-        child: Text(
-          '${changeNotifierCounter.value}',
-          style: const TextStyle(fontSize: 40.0),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            '${ref.read(changeNotifierProvider).slider}',
+            style: const TextStyle(fontSize: 40.0),
+          ),
+          Slider(
+              value: changeNotifierCounter.slider.toDouble(),
+              min: 0.0,
+              max: 100.0,
+              onChanged: (value) {
+                ref.read(changeNotifierProvider).sliderValue(value.toInt());
+              }),
+          Text(
+            '${changeNotifierCounter.value}',
+            style: const TextStyle(fontSize: 40.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                child: const Text('Increase 10'),
+                onPressed: () {
+                  ref.read(changeNotifierProvider).increamentBy10();
+                },
+              ),
+              ElevatedButton(
+                child: const Text('Decrease 10'),
+                onPressed: () {
+                  ref.read(changeNotifierProvider).decreamentBy10();
+                },
+              ),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -43,10 +75,32 @@ class ChangeNotifierClass extends ChangeNotifier {
   ChangeNotifierClass() : super();
 
   int value = 0;
+  int slider = 0;
+
+  void sliderValue(int value) {
+    slider = value;
+    notifyListeners();
+  }
 
   void increament() {
     value++;
     notifyListeners();
+  }
+
+  void increamentBy10() {
+    // value = value + 10;
+    /* we can also do this something like this but
+     the lower line syntax is more convenient way of doing this.
+    */
+    value += 10;
+    notifyListeners();
+  }
+
+  void decreamentBy10() {
+    if (value >= 10) {
+      value -= 10;
+      notifyListeners();
+    }
   }
 }
 
--- lib/state_notifier_provider.dart
+++ lib/state_notifier_provider.dart
@@ -6,32 +6,53 @@ class StateNotifierScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final stateNotifierCounter = ref.watch(stateNotifierProvider);
+    final stateNotifierCount = ref.watch(stateNotifierProvider);
+    final slider = ref.watch(sliderNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('State Notifier Screen'),
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back_ios)),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
         actions: [
           IconButton(
-              onPressed: () {
-                ref.invalidate(stateNotifierProvider);
-              },
-              icon: const Icon(Icons.refresh))
+            onPressed: () {
+              ref.invalidate(stateNotifierProvider);
+              ref.invalidate(sliderNotifierProvider);
+            },
+            icon: const Icon(Icons.refresh),
+          ),
         ],
       ),
-      body: Center(
-        child: Text(
-          '$stateNotifierCounter',
-          style: const TextStyle(fontSize: 40.0),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$stateNotifierCount',
+            style: const TextStyle(fontSize: 40.0),
+          ),
+          Text('$slider'),
+          Slider(
+            value:
+                ref.read(sliderNotifierProvider.notifier).slideCount.toDouble(),
+            min: 0.0,
+            max: 100.0,
+            onChanged: (value) {
+              ref.read(sliderNotifierProvider.notifier).sliderCount(
+                    value.toInt(),
+                  );
+              print('$slider');
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          ref.read(stateNotifierProvider.notifier).increament();
+          ref.read(stateNotifierProvider.notifier).increment();
         },
         child: const Icon(Icons.add),
       ),
@@ -41,13 +62,31 @@ class StateNotifierScreen extends ConsumerWidget {
 
 class StateNotifierClass extends StateNotifier<int> {
   StateNotifierClass() : super(0);
-  void increament() {
+
+  void increment() {
     state++;
   }
 }
 
 final stateNotifierProvider =
     StateNotifierProvider.autoDispose<StateNotifierClass, int>(
-        (ref) => StateNotifierClass());
+  (ref) => StateNotifierClass(),
+);
 
 // Here by using .autoDispose we are clearing the data of our counter after leaving the current page.
+
+class SliderNotifier extends StateNotifier<int> {
+  SliderNotifier() : super(0);
+
+  int slideCount = 1;
+
+  void sliderCount(int value) {
+    slideCount = value;
+    state = slideCount;
+  }
+  /* Here we can also directly assign the value to state also but 
+     i have done this via this variable slideCount */
+}
+
+final sliderNotifierProvider =
+    StateNotifierProvider<SliderNotifier, int>((ref) => SliderNotifier());
